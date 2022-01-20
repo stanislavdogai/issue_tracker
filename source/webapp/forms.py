9521dcd3c598ed9f9bcc061970728d217f1617d7
stanislavdogai <stanislavdogai01@gmail.com>
2022-01-20 17:43:25 +0600

    Добавил валидации к полям, обновил фикстуры

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,12 +1,45 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.forms import widgets
-from webapp.models import Type, Status
+from webapp.models import Type, Status, Task
 
-class TaskForm(forms.Form):
-    summary = forms.CharField(max_length=200, required=True, label="Заголовок")
-    description = forms.CharField(max_length=2002, required=False, label="Описание", widget=widgets.Textarea(attrs={"rows": 5, "cols":50}))
-    types = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), widget=forms.CheckboxSelectMultiple)
-    status = forms.ModelChoiceField(queryset=Status.objects.all())
+# class TaskForm(forms.Form):
+#     summary = forms.CharField(max_length=200, required=True, label="Заголовок")
+#     description = forms.CharField(max_length=2002, required=False, label="Описание", widget=widgets.Textarea(attrs={"rows": 5, "cols":50}))
+#     types = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), widget=forms.CheckboxSelectMultiple)
+#     status = forms.ModelChoiceField(queryset=Status.objects.all())
 
 class TaskFormDelete(forms.Form):
     confirm = forms.CharField(max_length=3, required=True, label='Подтверждение удаления')
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        exclude = []
+        widgets = {
+            'types' : forms.CheckboxSelectMultiple
+        }
+
+    def clean_summary(self):
+        if len(self.cleaned_data.get('summary')) < 8:
+            raise ValidationError(f'Длина заголовка должна быть больше 8 символов')
+        return self.cleaned_data.get('summary')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data.get('summary') == cleaned_data.get('description'):
+            raise ValidationError('Заголовок и описание не могут быть одинаковыми')
+        return cleaned_data
+
+    def clean_description(self):
+        # cleaned_data = super.clean()
+        if 'description' in self.cleaned_data.get('description'):
+            raise ValidationError('Нельзя вводить в поле description слово "description"')
+        return self.cleaned_data.get('description')
+
+    def clean_types(self):
+        print(len(self.cleaned_data.get('types')))
+        if len(self.cleaned_data.get('types')) == 3:
+            raise ValidationError('Нельзя выбирать все три типа')
+        return self.cleaned_data.get("types")
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -30,9 +30,10 @@ class CreateTask(CustomFormView):
     template_name = 'create_task.html'
 
     def form_valid(self, form):
-        types = form.cleaned_data.pop('types')
-        self.object = Task.objects.create(**form.cleaned_data)
-        self.object.types.set(types)
+        # types = form.cleaned_data.pop('types')
+        # self.object = Task.objects.create(**form.cleaned_data)
+        # self.object.types.set(types)
+        self.object = form.save()
         return super().form_valid(form)
 
     def get_redirect_url(self):
@@ -46,27 +47,33 @@ class UpdateTask(FormView):
 
     def dispatch(self, request, *args, **kwargs):
         self.task = self.get_object()
-        return super(UpdateTask, self).dispatch(request, *args, **kwargs)
+        return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['task'] = self.task
         return context
 
-    def get_initial(self):
-        initial = {}
-        for key in 'summary', 'description', 'status':
-            initial[key] = getattr(self.task, key)
-        initial['types'] = self.task.types.all()
-        return initial
+    # def get_initial(self):
+    #     initial = {}
+    #     for key in 'summary', 'description', 'status':
+    #         initial[key] = getattr(self.task, key)
+    #     initial['types'] = self.task.types.all()
+    #     return initial
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = self.task
+        return kwargs
 
     def form_valid(self, form):
-        types = form.cleaned_data.pop('types')
-        for key, value in form.cleaned_data.items():
-            if value is not None:
-                setattr(self.task, key, value)
-        self.task.save()
-        self.task.types.set(types)
+        # types = form.cleaned_data.pop('types')
+        # for key, value in form.cleaned_data.items():
+        #     if value is not None:
+        #         setattr(self.task, key, value)
+        # self.task.save()
+        # self.task.types.set(types)
+        self.task = form.save()
         return super(UpdateTask, self).form_valid(form)
 
     def get_success_url(self):
