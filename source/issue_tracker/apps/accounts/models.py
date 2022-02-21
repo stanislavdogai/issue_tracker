6705b31680c7f0957470d254a1a6a1d4cbd0bcb5
stanislavdogai <stanislavdogai01@gmail.com>
2022-02-21 18:09:52 +0600

    Небольшие корректировки в HTML коде

--- source/issue_tracker/apps/accounts/models.py
+++ source/issue_tracker/apps/accounts/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), related_name='profile', verbose_name='Профиль', on_delete=models.CASCADE)
-    avatar = models.ImageField(verbose_name='Аватар', upload_to='avatars/', null=True, blank=True)
+    avatar = models.ImageField(verbose_name='Аватар', upload_to='avatars/', null=False, blank=True)
     about = models.TextField(max_length=2000, null=True, blank=True, verbose_name="О себе",)
     github = models.CharField(max_length=50, null=True, blank=True, verbose_name='Ссылка на гит')
 
--- source/issue_tracker/settings.py
+++ source/issue_tracker/settings.py
@@ -120,15 +120,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_ROOT = BASE_DIR.joinpath('uploads')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+# MEDIA_ROOT = BASE_DIR.join.path('uploads')
 MEDIA_URL = '/uploads/'
 
 LOGIN_REDIRECT_URL = 'webapp:home_page'
